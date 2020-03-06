7edb8f15cc84155199996417337491af7bd09056
TinaCXu <xuchangtina@163.com>
2020-03-07 00:56:11 +0800

    python basics-1

---
+++ python/studyNote.py
@@ -0,0 +1,34 @@
+a = 21
+b = 10
+c = 0
+print (a ** c)
+print (a // b)
+print (a % b)
+print (a / b)
+
+#Python logical operators
+print (a and b)
+print (c and b)
+print (a or b)
+print (c or b)
+print (not(c or b))
+
+i = 2
+j = 2
+print(i % j)
+print(i % j == 0)
+print (not(i % j))
+
+#If Loop
+var = 100 
+if ( var  == 100 ): 
+    print ("变量 var 的值为100")
+else:
+    print ("Good bye!")
+
+#range & len
+fruits = ['banana', 'apple',  'mango']
+print (len(fruits)) #3
+print (range(len(fruits))) #range(0,3)
+range(3)
+range(0,1,2)
