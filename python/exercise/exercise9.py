e5499caf3c206481d7030ff6a3d4afdb7faad9bd
TinaCXu <xuchangtina@163.com>
2020-03-12 16:13:12 +0800

    python exercise

--- python/exercise/exercise9.py
+++ python/exercise/exercise9.py
@@ -3,15 +3,14 @@ standard = random.randint(1,9)
 number = input("Please guess the random number: ")
 number = int(number)
 i = 1
-if (number < standard):
-    print ("too low")
-    i += 1
-number = input("Please guess the random number again: ")
-number = int(number)
-if (number > standard):
-    print ("too high")
-    i += 1
-number = input("Please guess the random number again: ")
-number = int(number)
-if (number == standard):
-    print ("Congratulation! You have guessed ",i, " times!")
+while number != standard:
+    if (number < standard):
+        print ("too low")
+        i += 1
+    if (number > standard):
+        print ("too high")
+        i += 1
+    number = input("Please guess the random number again: ")
+    number = int(number)
+#if (number == standard): (jump out the while loop only when number equals to standard)
+print ("Congratulation! You have guessed ",i, " times!")
