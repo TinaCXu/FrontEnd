718ac52248a7631d2f7db410937c3f34650696fd
TinaCXu <xuchangtina@163.com>
2020-03-12 15:38:32 +0800

    Update exercise9.py

--- python/exercise/exercise9.py
+++ python/exercise/exercise9.py
@@ -1,2 +1,17 @@
 import random
-print(random.randint(1,9))
+standard = random.randint(1,9)
+number = input("Please guess the random number: ")
+number = int(number)
+i = 1
+if (number < standard):
+    print ("too low")
+    i += 1
+number = input("Please guess the random number again: ")
+number = int(number)
+if (number > standard):
+    print ("too high")
+    i += 1
+number = input("Please guess the random number again: ")
+number = int(number)
+if (number == standard):
+    print ("Congratulation! You have guessed ",i, " times!")
