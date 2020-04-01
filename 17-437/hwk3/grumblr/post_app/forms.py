885000a4f916b0ae119ccf24bbecefc520c13975
TinaCXu <xuchangtina@163.com>
2020-04-02 01:24:44 +0800

    hwk3-frontend

--- 17-437/hwk3/grumblr/post_app/forms.py
+++ 17-437/hwk3/grumblr/post_app/forms.py
@@ -3,13 +3,13 @@ from django.contrib.auth.models import User
 from .models import UserProfileInfo
 
 class RegisterForm(forms.Form):
-    username = forms.CharField()
-    first_name = forms.CharField()
-    last_name = forms.CharField()
-    email = forms.EmailField()
-    password = forms.CharField()
-    verify_password = forms.CharField()
-    introduction = forms.CharField()
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    verify_password = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    introduction = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control input-sm'}))
 
     def clean(self):
         #super(RegistrationForm, self).clean()
@@ -24,6 +24,10 @@ class LoginForm(forms.ModelForm):
     class Meta():
         model = User
         fields = ('username','password')
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}),
+            'password': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Password', 'width':'253.33px'}),
+        }
 
 class UserForm(forms.ModelForm):
     class Meta():
